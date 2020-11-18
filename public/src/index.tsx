aaa684fb5b985a88a9e8f9a41bb472c3b66e2f62
a.bogdanov <a.bogdanov@netex.pro>
2020-11-18 12:16:18 +0300

    minor cleanup

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+import './index.css';
 
 const root = document.getElementById('root');
 
-// Add this import:
-
-// Wrap the rendering in a function:
 const render = (Component: any) => {
   ReactDOM.render(
-    // Wrap App inside AppContainer
     <AppContainer>
       <Component />
     </AppContainer>,
@@ -20,13 +18,10 @@ const render = (Component: any) => {
   );
 };
 
-// Do this once
 serviceWorker.register();
 
-// Render once
 render(App);
 
-// Webpack Hot Module Replacement API
 if (module.hot) {
   module.hot.accept('./App', () => {
     render(App);
